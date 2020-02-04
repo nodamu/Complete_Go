0dcc184420f87a2e28e2d83934b7881c0e2882c6
nodamu <nickadamu@gmail.com>
2020-02-04 03:58:38 -0800

    renamed new card func to newDeck and moved to deck

--- Cards/deck.go
+++ Cards/deck.go
@@ -10,3 +10,19 @@ func (d deck) print() {
 		fmt.Println(i, card)
 	}
 }
+
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuites := []string{"Spades"."Diamonds","Hearts","Clubs"}
+	cardValues := []string{"Ace","Two","Three","Four"}
+
+	for i, suit := range cardSuites {
+		for j, value := range cardValues{
+			cards = append(cards, suit+" of "+"value")
+		}
+	}
+
+	return cards
+
+}
--- Cards/main.go
+++ Cards/main.go
@@ -1,24 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Ace of spade", newCard(), newCard()}
-	cards = append(cards, "Jack")
-
+	cards := newDeck()
 	cards.print()
-
-}
-
-func newCard() deck {
-	cards := deck{}
-	cardSuites := []string{"Spades"."Diamonds","Hearts","Clubs"}
-	cardValues := []string{"Ace","Two","Three","Four"}
-
-	for i, suit := range cardSuites {
-		for j, value := range cardValues{
-			cards = append(cards, suit+" of "+"value")
-		}
-	}
-
-	return cards
 
 }
