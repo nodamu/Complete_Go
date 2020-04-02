6cb785281cf0514db0db1516fd1f41d829b6add4
nodamu <nickadamu@gmail.com>
2020-04-02 13:31:46 +0000

    Added custom writer implementation

--- http/main.go
+++ http/main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
 
+type logWriter struct {
+}
+
 func main() {
 	resp, err := http.Get("http://google.com")
+
+	lw := logWriter{}
 
 	if err != nil {
 		fmt.Println("Error", err)
 		os.Exit(1)
 	}
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
 
-	fmt.Println(resp)
+	// fmt.Println(string(bs))
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Returned ", len(bs), "number of bytes")
+	return len(bs), nil
 }
